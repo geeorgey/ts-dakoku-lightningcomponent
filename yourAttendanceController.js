99d879c3a8da567afc85bcbd9d6002f2c1e2ff99
George Yoshida <y@lne.st>
2018-11-07 17:39:45 +0900

    js controller

---
+++ yourAttendanceController.js
@@ -0,0 +1,8 @@
+({
+	doInit : function(component, event, helper) {
+        helper.isSetYourTime(component);
+	},
+	updateAttendance : function(component, event, helper)  {
+        helper.updateAttendance_helper(component) ;
+	},
+})
